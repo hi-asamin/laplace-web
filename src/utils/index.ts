e4e154dac0152014ee8cbcf41ce522d1e400ba0e
hi-asamin <asamin.itconsultant@gmail.com>
2025-05-11 00:33:48 +0900

    シミュレーションページの結果表示実装

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getFlagIcon(market: string) {
+  switch (market?.toLowerCase()) {
+    case 'japan':
+      return '/flags/japan.svg';
+    case 'us':
+      return '/flags/us.svg';
+    default:
+      return '/flags/global.svg';
+  }
+}

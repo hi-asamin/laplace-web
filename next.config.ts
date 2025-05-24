951f57a45cffbd3640f85ff4dcada4a4044b4e58
kota <no.shaking.throne974zero@gmail.com>
2025-05-24 21:46:33 +0900

    Vercelの画像最適化をオフ

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // 画像の最適化を無効化
+  }  /* config options here */
 };
 
 export default nextConfig;
